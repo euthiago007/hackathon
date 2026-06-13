dd0f6669d1729e8d5fdc7d05a9dbc425908f117b
ritamacedk <ritaaa.amador@gmail.com>
2026-06-13 16:07:19 -0300

    Visualizacao das vagas e candidaturas pronta

---
+++ portal-faculdade/src/main/java/com/unialfa/model/Vaga.java
@@ -0,0 +1,68 @@
+package com.unialfa.model;
+
+public class Vaga {
+
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private String requisitos;
+    private Double bolsa;
+    private Boolean ativa;
+    private Long empresaId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getRequisitos() {
+        return requisitos;
+    }
+
+    public void setRequisitos(String requisitos) {
+        this.requisitos = requisitos;
+    }
+
+    public Double getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(Double bolsa) {
+        this.bolsa = bolsa;
+    }
+
+    public Boolean getAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(Boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public Long getEmpresaId() {
+        return empresaId;
+    }
+
+    public void setEmpresaId(Long empresaId) {
+        this.empresaId = empresaId;
+    }
+}
